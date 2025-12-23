d2d9b0d9bc8bc60b5602eb8ee6829344b65074bd
NurulIslam17 <nurulcse09@gmail.com>
2025-12-23 14:33:43 +0600

    Validation

--- src/main/java/com/nurul/RestAPIs/dto/AddStudentRequestDto.java
+++ src/main/java/com/nurul/RestAPIs/dto/AddStudentRequestDto.java
@@ -1,8 +1,16 @@
 package com.nurul.RestAPIs.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class AddStudentRequestDto {
 
+    @NotBlank(message = "Name is required")
+    @Size(min = 2,max = 15, message = "Name Should be of length 3 to 15 characters.")
     private String name;
+    @Email
+    @NotBlank(message = "Email is required.")
     private String email;
 
     public AddStudentRequestDto(String name, String email) {
