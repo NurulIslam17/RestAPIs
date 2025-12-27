d3505b7641dfc0a86aacf98b822ff373d316ae71
NurulIslam17 <nurulcse09@gmail.com>
2025-12-27 16:29:56 +0600

    Request validation handle

---
+++ src/main/java/com/nurul/RestAPIs/security/JwtAuthFilter.java
@@ -0,0 +1,50 @@
+package com.nurul.RestAPIs.security;
+
+import com.nurul.RestAPIs.entity.User;
+import com.nurul.RestAPIs.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final UserRepository userRepository;
+    private final AuthUtil authUtil;
+
+    public JwtAuthFilter(UserRepository userRepository, AuthUtil authUtil) {
+        this.userRepository = userRepository;
+        this.authUtil = authUtil;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        final String requestTokenHeader = request.getHeader("Authorization");
+        if (requestTokenHeader == null || requestTokenHeader.startsWith("Bearer")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String token = requestTokenHeader.split("Bearer")[1];
+        String userName = authUtil.getUserNameFromToken(token);
+
+        if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null)
+        {
+            User user = userRepository.findByUserName(userName).orElseThrow();
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+        }
+        filterChain.doFilter(request,response);
+
+    }
+}
--- src/main/java/com/nurul/RestAPIs/security/WebSecurityConfig.java
+++ src/main/java/com/nurul/RestAPIs/security/WebSecurityConfig.java
@@ -1,14 +1,19 @@
 package com.nurul.RestAPIs.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class WebSecurityConfig {
+
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
@@ -17,7 +22,8 @@ public class WebSecurityConfig {
                 .sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/public/**", "/auth/**").permitAll()
-                        .requestMatchers("/admin/**").hasRole("ADMIN"));
+                        .anyRequest().authenticated())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
 }
