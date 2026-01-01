de608df279fd6634824ba9dbe38ccb926678d4ff
NurulIslam17 <nurulcse09@gmail.com>
2026-01-01 14:18:58 +0600

    Registraion mail

--- src/main/java/com/nurul/RestAPIs/config/AppConfig.java
+++ src/main/java/com/nurul/RestAPIs/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.nurul.RestAPIs.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
