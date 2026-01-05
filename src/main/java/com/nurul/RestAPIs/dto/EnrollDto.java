51218cd5877c5afecce48e47c79e8be19fbc6e66
NurulIslam17 <nurulcse09@gmail.com>
2026-01-05 15:46:48 +0600

    Enroll

--- src/main/java/com/nurul/RestAPIs/dto/EnrollDto.java
+++ src/main/java/com/nurul/RestAPIs/dto/EnrollDto.java
@@ -1,4 +1,6 @@
 package com.nurul.RestAPIs.dto;
+import com.nurul.RestAPIs.entity.Course;
+import com.nurul.RestAPIs.entity.Student;
 import com.nurul.RestAPIs.entity.type.EnrollmentStatus;
 import lombok.*;
 
@@ -8,13 +10,8 @@ import java.time.LocalDateTime;
 public class EnrollDto {
 
     private Long id;
-    private Long studentId;
-    private String studentName;
-    private String studentEmail;
-    private Long courseId;
-    private String courseTitle;
-    private String courseCode;
-    private String teacherName;
+    private Student student;
+    private Course course;
     private EnrollmentStatus enrollmentStatus;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
@@ -23,15 +20,10 @@ public class EnrollDto {
 
     }
 
-    public EnrollDto(Long id, Long studentId, String studentName, String studentEmail, Long courseId, String courseTitle, String courseCode, String teacherName, EnrollmentStatus enrollmentStatus, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    public EnrollDto(Long id, Student student, Course course, EnrollmentStatus enrollmentStatus, LocalDateTime createdAt, LocalDateTime updatedAt) {
         this.id = id;
-        this.studentId = studentId;
-        this.studentName = studentName;
-        this.studentEmail = studentEmail;
-        this.courseId = courseId;
-        this.courseTitle = courseTitle;
-        this.courseCode = courseCode;
-        this.teacherName = teacherName;
+        this.student = student;
+        this.course = course;
         this.enrollmentStatus = enrollmentStatus;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
@@ -45,60 +37,20 @@ public class EnrollDto {
         this.id = id;
     }
 
-    public Long getStudentId() {
-        return studentId;
+    public Student getStudent() {
+        return student;
     }
 
-    public void setStudentId(Long studentId) {
-        this.studentId = studentId;
+    public void setStudent(Student student) {
+        this.student = student;
     }
 
-    public String getStudentName() {
-        return studentName;
+    public Course getCourse() {
+        return course;
     }
 
-    public void setStudentName(String studentName) {
-        this.studentName = studentName;
-    }
-
-    public String getStudentEmail() {
-        return studentEmail;
-    }
-
-    public void setStudentEmail(String studentEmail) {
-        this.studentEmail = studentEmail;
-    }
-
-    public Long getCourseId() {
-        return courseId;
-    }
-
-    public void setCourseId(Long courseId) {
-        this.courseId = courseId;
-    }
-
-    public String getCourseTitle() {
-        return courseTitle;
-    }
-
-    public void setCourseTitle(String courseTitle) {
-        this.courseTitle = courseTitle;
-    }
-
-    public String getCourseCode() {
-        return courseCode;
-    }
-
-    public void setCourseCode(String courseCode) {
-        this.courseCode = courseCode;
-    }
-
-    public String getTeacherName() {
-        return teacherName;
-    }
-
-    public void setTeacherName(String teacherName) {
-        this.teacherName = teacherName;
+    public void setCourse(Course course) {
+        this.course = course;
     }
 
     public EnrollmentStatus getEnrollmentStatus() {
