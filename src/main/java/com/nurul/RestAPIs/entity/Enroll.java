510a7ee6fe7146537d920274d1722791fee864d4
NurulIslam17 <nurulcse09@gmail.com>
2026-01-06 11:40:35 +0600

    Enrollment fetch

--- src/main/java/com/nurul/RestAPIs/entity/Enroll.java
+++ src/main/java/com/nurul/RestAPIs/entity/Enroll.java
@@ -2,19 +2,10 @@ package com.nurul.RestAPIs.entity;
 
 import com.nurul.RestAPIs.entity.type.EnrollmentStatus;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.time.LocalDateTime;
 
-@Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 
+@Entity
 public class Enroll {
 
     @Id
@@ -38,4 +29,63 @@ public class Enroll {
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
+    public Enroll() {
+    }
+
+    public Enroll(Long id, Student student, Course course, EnrollmentStatus enrollmentStatus, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.student = student;
+        this.course = course;
+        this.enrollmentStatus = enrollmentStatus;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public EnrollmentStatus getEnrollmentStatus() {
+        return enrollmentStatus;
+    }
+
+    public void setEnrollmentStatus(EnrollmentStatus enrollmentStatus) {
+        this.enrollmentStatus = enrollmentStatus;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
