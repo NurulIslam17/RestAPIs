5ac9443a2fe983ef5b8d69864fd6e5ade9ae3628
NurulIslam17 <nurulcse09@gmail.com>
2026-01-06 12:56:06 +0600

    Enrolment post

--- src/main/java/com/nurul/RestAPIs/dto/EnrollRequestDto.java
+++ src/main/java/com/nurul/RestAPIs/dto/EnrollRequestDto.java
@@ -1,17 +1,26 @@
 package com.nurul.RestAPIs.dto;
 
+import com.nurul.RestAPIs.entity.type.EnrollmentStatus;
+import jakarta.persistence.Column;
+
+import java.time.LocalDateTime;
+
 public class EnrollRequestDto {
     private Long studentId;
     private Long courseId;
-    private String enrollmentStatus;
+    private EnrollmentStatus enrollmentStatus;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
     public EnrollRequestDto() {
     }
 
-    public EnrollRequestDto(Long studentId, Long courseId, String enrollmentStatus) {
+    public EnrollRequestDto(Long studentId, Long courseId, EnrollmentStatus enrollmentStatus, LocalDateTime createdAt, LocalDateTime updatedAt) {
         this.studentId = studentId;
         this.courseId = courseId;
         this.enrollmentStatus = enrollmentStatus;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 
     public Long getStudentId() {
@@ -30,11 +39,27 @@ public class EnrollRequestDto {
         this.courseId = courseId;
     }
 
-    public String getEnrollmentStatus() {
+    public EnrollmentStatus getEnrollmentStatus() {
         return enrollmentStatus;
     }
 
-    public void setEnrollmentStatus(String enrollmentStatus) {
+    public void setEnrollmentStatus(EnrollmentStatus enrollmentStatus) {
         this.enrollmentStatus = enrollmentStatus;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
     }
 }
--- src/main/java/com/nurul/RestAPIs/service/EnrollService.java
+++ src/main/java/com/nurul/RestAPIs/service/EnrollService.java
@@ -1,8 +1,13 @@
 package com.nurul.RestAPIs.service;
 
 import com.nurul.RestAPIs.dto.EnrollDto;
+import com.nurul.RestAPIs.dto.EnrollRequestDto;
+import com.nurul.RestAPIs.entity.Course;
 import com.nurul.RestAPIs.entity.Enroll;
+import com.nurul.RestAPIs.entity.Student;
+import com.nurul.RestAPIs.repository.CourseRepository;
 import com.nurul.RestAPIs.repository.EnrollRepository;
+import com.nurul.RestAPIs.repository.StudentRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +17,14 @@ import java.util.List;
 public class EnrollService {
 
     private final EnrollRepository enrollRepository;
+    private final StudentRepository studentRepository;
+    private final CourseRepository courseRepository;
     private final ModelMapper modelMapper;
 
-    public EnrollService(EnrollRepository enrollRepository, ModelMapper modelMapper) {
+    public EnrollService(EnrollRepository enrollRepository, CourseService courseService, StudentRepository studentRepository, CourseRepository courseRepository, ModelMapper modelMapper) {
         this.enrollRepository = enrollRepository;
+        this.studentRepository = studentRepository;
+        this.courseRepository = courseRepository;
         this.modelMapper = modelMapper;
     }
 
@@ -26,5 +35,21 @@ public class EnrollService {
                 .stream()
                 .map(enroll -> modelMapper.map(enroll, EnrollDto.class))
                 .toList();
+    }
+
+    public EnrollDto save(EnrollRequestDto enrollRequestDto) {
+
+        Student student = studentRepository.findById(enrollRequestDto.getStudentId()).orElseThrow(() -> new IllegalArgumentException("Student not found"));
+        Course course = courseRepository.findById(enrollRequestDto.getCourseId()).orElseThrow(() -> new IllegalArgumentException("Course not found"));
+
+        Enroll enroll = new Enroll();
+        enroll.setStudent(student);
+        enroll.setCourse(course);
+        enroll.setCreatedAt(enrollRequestDto.getCreatedAt());
+        enroll.setCreatedAt(enrollRequestDto.getUpdatedAt());
+        enroll.setEnrollmentStatus(enrollRequestDto.getEnrollmentStatus());
+
+        Enroll saveEnroll = enrollRepository.save(enroll);
+        return modelMapper.map(saveEnroll, EnrollDto.class);
     }
 }
